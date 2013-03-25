a2a17161260b30bb44aebc3653f56aaffb007660
Eric Merriman <eric34@gmail.com>
2013-03-24 23:10:10 -0700

    Added more detail in the comments

--- Resources/PositionData.js
+++ Resources/PositionData.js
@@ -1,26 +1,26 @@
 
-// This file will manage waypoint data
-
+// This file will manage waypoint data if I can get to the "add waypoint" feature
 var pd = {};
 module.exports = pd;
 
-// this will add the list of items to the properties
+// this will add the list of default waypoints to the properties when the app is first launched
+// maybe I should just keep the default waypoints hardcoded, and make this do that
 pd.warmUp = function() {
 	
 }
 
-// This function will check the data to see if it's been inited.
+// This function will check the data to see if there are any user-created waypoints. If so, add them to the waypoint tableview, but in another section (group)
 pd.checkStatus = function() {
 	// if not inited, call warm up
 }
 
 
-// This will add a waypoint
+// This will add a user waypoint
 pd.add =function (waypoint) {
 	
 }
 
-// This will remove a waypoint
+// This will remove a user waypoint
 pd.remove = function (waypoint) {
 	
 }
